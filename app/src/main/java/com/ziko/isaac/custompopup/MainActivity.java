3cd2e5185bb5bf5b7caeb0392041781680852e46
zikob <zikobooking@gmail.com>
2020-07-09 12:50:13 +0300

    Finished

--- app/src/main/java/com/ziko/isaac/custompopup/MainActivity.java
+++ app/src/main/java/com/ziko/isaac/custompopup/MainActivity.java
@@ -2,15 +2,43 @@ package com.ziko.isaac.custompopup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Dialog myDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        myDialog = new Dialog(this);
+
+
+    }
+
+    public void showPopup(View view) {
+        TextView txtClose;
+        Button btnFollow;
+        myDialog.setContentView(R.layout.custom_popup);
+        txtClose = myDialog.findViewById(R.id.close);
+        btnFollow = myDialog.findViewById(R.id.follow_me);
+
+        txtClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 }
