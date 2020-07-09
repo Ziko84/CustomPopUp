cbd0b3b5fffaf6cf26efceec4cfbba6455fd2906
zikob <zikobooking@gmail.com>
2020-07-09 10:42:02 +0300

    Initial commit Popup Screen

---
+++ app/src/main/java/com/ziko/isaac/custompopup/MainActivity.java
@@ -0,0 +1,16 @@
+package com.ziko.isaac.custompopup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
